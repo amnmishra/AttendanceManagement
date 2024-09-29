d877561b18c12648e0a112f6eca9499ebc698905
AMAN MISHRA <101791682+amnmishra@users.noreply.github.com>
2024-09-29 17:49:18 +0530

    IV

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,10 +3,7 @@ require('dotenv').config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      
-      
-    });
+    await mongoose.connect('mongodb+srv://kk:Attendance0101%23@attendanceapp.bq02n.mongodb.net/?retryWrites=true&w=majority&appName=AttendanceApp');
     console.log('MongoDB Connected');
   } catch (error) {
     console.error('Database connection error:', error);
